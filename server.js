03fdfd9f28957348e2216e54045e6451ce05c237
alexm <alex-m@outlook.com>
2023-04-18 10:07:50 -0700

    Remove database connection test code

--- server.js
+++ server.js
@@ -10,13 +10,3 @@ const { app } = require("./app");
 app.listen(3000, "127.0.0.1", () => {
     console.log("Listening on 127.0.0.1, port 3000");
 });
-
-// const { Client } = require('pg')
-// const client = new Client()
-
-// client.connect()
-
-// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-//   client.end()
-// })
