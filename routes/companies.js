5baf7f8bc4b9b7919fa71c6b013f53f00a88f10e
alexm <alex-m@outlook.com>
2023-04-18 14:34:12 -0700

    Import ExpressError; add GET route for specific company

--- routes/companies.js
+++ routes/companies.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const router = express.Router();
 
+const { ExpressError } = require("../expressError");
 const { db } = require("../db");
 
 
@@ -15,7 +16,7 @@ router.get("/", async (req, res, next) => {
 
     try {
         const results = await db.query(
-            `SELECT * FROM companies`
+            `SELECT code, name, description FROM companies`
         );
 
         return res.json({companies: results.rows});
@@ -24,6 +25,34 @@ router.get("/", async (req, res, next) => {
         return next(err);
     }
 })
+
+
+/**
+ * Get a specific company, by company code: {company: {code, name, description}}
+ *
+ * If company cannot be found, return 404 status response.
+ */
+router.get("/:code", async (req, res, next) => {
+
+    try {
+        const result = await db.query(
+            `SELECT code, name, description FROM companies
+             WHERE code = $1`,
+             [req.params.code]
+        );
+
+        // Go to global error handler if company not found
+        if (result.rows.length === 0) {
+            throw new ExpressError("Company not found!", 404);
+        }
+
+        return res.json({company: result.rows[0]});
+
+    } catch(err) {
+        return next(err);
+    }
+})
+
 
 
 module.exports = {
