84e52bac678a395436fb4160d8374d3c809b8154
alexm <alex-m@outlook.com>
2023-04-18 15:56:48 -0700

    Add GET route to get all invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -9,6 +9,25 @@ const { ExpressError } = require("../expressError");
 const { db } = require("../db");
 
 
+/**
+ * Get list of all invoices: {invoices: [{id, comp_code}, ...]}
+ */
+router.get("/", async (req, res, next) => {
+
+    try {
+        const results = await db.query(
+            `SELECT id, comp_code FROM invoices`
+        );
+
+        return res.json({invoices: results.rows});
+
+    } catch(err) {
+        return next(err);
+    }
+})
+
+
+
 module.exports = {
     router
 };
