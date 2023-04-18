8cab7d91331352bc185bedde94baecbdadbcf419
alexm <alex-m@outlook.com>
2023-04-17 19:14:56 -0700

    Adjust indentation, spacing, and module export format; import and use companies router

--- app.js
+++ app.js
@@ -7,28 +7,31 @@
 const express = require("express");
 
 const app = express();
-const ExpressError = require("./expressError")
+const { router: companiesRoutes } = require("./routes/companies");
+const ExpressError = require("./expressError");
 
 app.use(express.json());
+app.use("/companies", companiesRoutes);
 
 
 /** 404 handler */
-
 app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
-/** general error handler */
 
+/** General error handler */
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    return res.json({
+        error: err,
+        message: err.message
+    });
 });
 
 
-module.exports = app;
+module.exports = {
+    app
+};
