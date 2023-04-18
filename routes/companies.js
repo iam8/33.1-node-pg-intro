c8fab3317621e347270efd87c3fceb752aae7029
alexm <alex-m@outlook.com>
2023-04-17 19:15:26 -0700

    Add imports; export router for companies

--- routes/companies.js
+++ routes/companies.js
@@ -1,2 +1,15 @@
 // Ioana A Mititean
 // Exercise 33.1: Node-pg Intro
+
+
+const express = require("express");
+const router = express.Router();
+
+const { db } = require("../db");
+
+
+
+
+module.exports = {
+    router
+};
