88d4abef98f059605b05b0b99e90c55aacaea9e4
alexm <alex-m@outlook.com>
2023-04-18 16:43:34 -0700

    Remove commented-out code for GET /id route

--- routes/invoices.js
+++ routes/invoices.js
@@ -49,27 +49,10 @@ router.get("/:id", async (req, res, next) => {
             throw new ExpressError("Invoice not found!", 404);
         }
 
+        // Construct and return response
         const {code, name, description, ...invData} = result.rows[0];
         invData.company = {code, name, description};
         return res.json(invData);
-
-        // const data = result.rows[0];
-        // const {id, amt, paid, add_date, paid_date} = result.rows[0];
-        // return res.json({id, amt, paid, add_date, paid_date});
-
-        // Construct and return response
-        // return res.json({
-        //     id: data.id,
-        //     amt: data.amt,
-        //     paid: data.paid,
-        //     add_date: data.add_date,
-        //     paid_date: data.paid_date,
-        //     company: {
-        //         code: data.code,
-        //         name: data.name,
-        //         description: data.description
-        //     }
-        // });
 
     } catch(err) {
         return next(err);
