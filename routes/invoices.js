4372e3591273490fc5ceeb9a8baee037903e4e30
alexm <alex-m@outlook.com>
2023-04-18 17:09:23 -0700

    Add PUT route to update an invoice; fix format of JSON response for POST and GET /:id routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -52,7 +52,7 @@ router.get("/:id", async (req, res, next) => {
         // Construct and return response
         const {code, name, description, ...invData} = result.rows[0];
         invData.company = {code, name, description};
-        return res.json(invData);
+        return res.json({invoice: invData});
 
     } catch(err) {
         return next(err);
@@ -79,12 +79,50 @@ router.post("/", async (req, res, next) => {
              [comp_code, amt]
         );
 
-        return res.status(201).json(result.rows[0]);
+        return res.status(201).json({invoice: result.rows[0]});
 
     } catch(err) {
         return next(err);
     }
 })
+
+
+/**
+ * Update an existing invoice.
+ *
+ * If invoice cannot be found, return 404 status response.
+ *
+ * Request body format (JSON): {amt}
+ *
+ * Return JSON with updated invoice info, if successful:
+ *      {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+ */
+router.put("/:id", async (req, res, next) => {
+
+    try {
+        const { amt } = req.body;
+        const result = await db.query(
+            `UPDATE invoices
+             SET amt = $1
+             WHERE id = $2
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+             [amt, req.params.id]
+        );
+
+        // Throw error if invoice not found
+        if (result.rows.length === 0) {
+            throw new ExpressError("Invoice not found!", 404);
+        }
+
+        return res.json({invoice: result.rows[0]});
+
+    } catch(err) {
+        return next(err);
+    }
+})
+
+
+
 
 
 module.exports = {
