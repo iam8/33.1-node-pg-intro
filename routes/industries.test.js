79e014915518f84b926a1760fcecf5136fe7ac1a
alexm <alex-m@outlook.com>
2023-04-19 19:05:27 -0700

    Add test setup code and import required modules

--- routes/industries.test.js
+++ routes/industries.test.js
@@ -4,3 +4,11 @@
 /**
  * Tests for /industries routes.
  */
+
+
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+
+const { app } = require("../app");
+const { db } = require("../db");
